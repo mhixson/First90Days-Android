01327b04b31b89d36bef695a0dfe8bfa0ecac9c0
mhixson <mhixson@gmail.com>
2013-06-03 14:52:42 -0700

    put the book title in italics

--- assets/www/js/buyTheBook.js
+++ assets/www/js/buyTheBook.js
@@ -7,7 +7,7 @@ var BuyTheBookPage = new Class ({
 	},
 	render: function () {
         var html = '<div id="article" class="content">' +
-                                    '<div id="buythebook-body">Purchase the 10th anniversary eBook edition of The First 90 Days via the links below.<br/><br/><br/>' + 
+                                    '<div id="buythebook-body">Purchase the 10th anniversary eBook edition of <i>The First 90 Days</i> via the links below.<br/><br/><br/>' + 
                                     '<a href="http://hbr.org/product/the-first-90-days-updated-and-expanded-proven-strategies-for-getting-up-to-speed-faster-and-smarter/an/11323E-KND-ENG">HBR.org</a><br/><br/><br/>' +
                                     '<a href="http://www.amazon.com/First-Days-Updated-Expanded-ebook/dp/B00B6U63ZE/">Kindle</a><br/><br/><br/>' +
                                     '<a href="http://www.barnesandnoble.com/w/the-first-90-days-updated-and-expanded-michael-watkins/1113628619">Nook</a></div>';

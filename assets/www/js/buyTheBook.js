1b34bc7dfb958f41e156effddac3d0c89860b2d8
mhixson <mhixson@gmail.com>
2013-06-10 14:00:15 -0700

    Buy the Book link now goes to Google Play

--- assets/www/js/buyTheBook.js
+++ assets/www/js/buyTheBook.js
@@ -8,9 +8,7 @@ var BuyTheBookPage = new Class ({
 	render: function () {
         var html = '<div id="article" class="content">' +
                                     '<div id="buythebook-body">Purchase the 10th anniversary eBook edition of <i>The First 90 Days</i> via the links below.<br/><br/><br/>' + 
-                                    '<a href="http://hbr.org/product/the-first-90-days-updated-and-expanded-proven-strategies-for-getting-up-to-speed-faster-and-smarter/an/11323E-KND-ENG">HBR.org</a><br/><br/><br/>' +
-                                    '<a href="http://www.amazon.com/First-Days-Updated-Expanded-ebook/dp/B00B6U63ZE/">Kindle</a><br/><br/><br/>' +
-                                    '<a href="http://www.barnesandnoble.com/w/the-first-90-days-updated-and-expanded-michael-watkins/1113628619">Nook</a></div>';
+                                    '<a href="https://play.google.com/store/books/details/Michael_Watkins_The_First_90_Days_Updated_and_Expa?id=QGkHs4pExOQC">Buy the Book</a><br/><br/><br/>';
 		var buyTheBookContent = new Element ('div', {
                             id: 'buyTheBookContent',
                             html: html
